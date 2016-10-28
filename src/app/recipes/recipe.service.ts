0723d3168e4b5bdd01fe54c1c328392749819362
William Headrick <headrick@utexas.edu>
2016-10-28 10:16:56 -0500

    began working on angular forms

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -14,7 +14,23 @@ export class RecipeService {
 
   constructor() { }
 
+  getRecipe(id: number){
+    return this.recipes[id];
+  }
+
   getRecipes(){
     return this.recipes;
+  }
+
+  deleteRecipe(recipe: Recipe){
+    this.recipes.splice(this.recipes.indexOf(recipe), 1);
+  }
+
+  addRecipe(recipe: Recipe){
+    this.recipes.push(recipe);
+  }
+
+  editRecipe(oldRecipe: Recipe, newRecipe: Recipe){
+    this.recipes[this.recipes.indexOf(oldRecipe)] = newRecipe;
   }
 }
--- src/app/shopping-list/shopping-list-add.component.ts
+++ src/app/shopping-list/shopping-list-add.component.ts
@@ -1,14 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnChanges, EventEmitter} from '@angular/core';
+import {Ingredient} from "../shared/ingredient";
+import {ShoppingListService} from "./shopping-list.service";
+import {Input, Output} from "@angular/core/src/metadata/directives";
 
 @Component({
   selector: 'rb-shopping-list-add',
   templateUrl: './shopping-list-add.component.html'
 })
-export class ShoppingListAddComponent implements OnInit {
+export class ShoppingListAddComponent implements OnChanges {
+  @Input() item: Ingredient;
+  @Output() cleared = new EventEmitter();
+  isAdd: boolean = true;
 
-  constructor() { }
+  constructor(private sls: ShoppingListService) { }
 
-  ngOnInit() {
+  ngOnChanges(changes){
+    if(changes.item.currentValue === null){
+      this.isAdd = true;
+      this.item = {name: '', amount: null};
+    } else {
+      console.log(this.item);
+      this.isAdd = false;
+    }
   }
 
+  onSubmit(ingredient: Ingredient){
+    var newIngredient = new Ingredient(ingredient.name, ingredient.amount);
+    if(!this.isAdd){
+      this.sls.editItem(this.item, newIngredient);
+      this.onClear();
+    } else {
+      this.item = newIngredient;
+      this.sls.addItem(this.item);
+    }
+  }
+
+  onDelete(){
+    this.sls.deleteItem(this.item);
+    this.onClear();
+  }
+
+  onClear(){
+    this.isAdd = true;
+    this.cleared.emit(null);
+  }
 }
